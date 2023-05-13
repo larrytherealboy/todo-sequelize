fb16c7e6c18788aa1f93cd1c1dd0c1175bdff894
larrytherealboy <abclarryabclarry@gmail.com>
2023-05-13 16:31:12 +0800

    feat: revise

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,16 +2,27 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => {
-      return res.render('index', { todos: todos })
+  const errors = []
+
+  User.findOne({ where: { email: req.user.email } })
+    .then(user => {
+      if (!user) {
+        console.log('user not found')
+      }
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: req.user.id }
+      })
+        .then((todos) => {
+          return res.render('index', { todos: todos })
+        })
+        .catch((error) => { return res.status(422).json(error) })
     })
-    .catch((error) => { return res.status(422).json(error) })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
